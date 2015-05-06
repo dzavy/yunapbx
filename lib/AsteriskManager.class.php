f8869e0fb56faff977212c5742e01d3c7e08be95
dzavy <dzavy@dzavy.net>
2015-05-06 22:17:26 +0000

    initial commit + nbproject

--- lib/AsteriskManager.class.php
+++ lib/AsteriskManager.class.php
@@ -1,4 +1,5 @@
 <?php
+
 define("AERR_CANNOT_CONNECT", 1);
 define("AERR_INVALID_SOCKET", 2);
 define("AERR_AUTHENTICATION_FAIL", 3);
@@ -6,83 +7,89 @@ define("AERR_AUTHENTICATION_FAIL", 3);
 require_once("AsteriskManagerMessage.class.php");
 
 class AsteriskManager {
-	private static $GetConfig_Cache;
-	private $Server;
-	private $Port;
-	private $Socket;
 
-	function __construct($Server = "127.0.0.1", $Port = "5038") {
-		$this->Server = $Server;
-		$this->Port   = $Port;
-	}
+    private static $GetConfig_Cache;
+    private $Server;
+    private $Port;
+    private $Socket;
 
-	function __destruct() {
-		if ($this->Socket) {
-			fclose($this->Socket);
-		}
-	}
+    function __construct($Server = "127.0.0.1", $Port = "5038") {
+        $this->Server = $Server;
+        $this->Port = $Port;
+    }
 
-	function Connect($User, $Pass) {
-		$this->Socket = fsockopen($this->Server, $this->Port, $errno, $errstr);
-		if (!$this->Socket) {
-			throw new Exception($errstr, constant("AERR_CANNOT_CONNECT"));
-		}
+    function __destruct() {
+        if ($this->Socket) {
+            fclose($this->Socket);
+        }
+    }
 
-		stream_set_timeout($this->Socket, 1);
+    function Connect($User, $Pass) {
+        $this->Socket = fsockopen($this->Server, $this->Port, $errno, $errstr);
+        if (!$this->Socket) {
+            throw new Exception($errstr, constant("AERR_CANNOT_CONNECT"));
+        }
 
-		$message = new AsteriskManagerMessage();
-		$message->SetKey("Action"  , "Login");
-		$message->SetKey("UserName", $User);
-		$message->SetKey("Secret"  , $Pass);
-		$message->SetKey("Events"  , "off");
+        stream_set_timeout($this->Socket, 1);
 
-		$response = $this->Send($message);
+        $message = new AsteriskManagerMessage();
+        $message->SetKey("Action", "Login");
+        $message->SetKey("UserName", $User);
+        $message->SetKey("Secret", $Pass);
+        $message->SetKey("Events", "off");
 
-		if ($response->GetKey("Response") != "Success") {
-			throw new Exception("Authentication failed", constant("AERR_AUTHENTICATION_FAIL"));
-		}
-	}
+        $response = $this->Send($message);
 
-	public function Send(AsteriskManagerMessage $query) {
-		if ($this->Socket == null) {
-			throw new Exception("Couldn't send query to Asterisk Manager. Null Socket.", constant("AERR_INVALID_SOCKET"));
-		}
+        if ($response->GetKey("Response") != "Success") {
+            throw new Exception("Authentication failed", constant("AERR_AUTHENTICATION_FAIL"));
+        }
+    }
 
-		//echo "<pre>{$query->ToString()}</pre>";
-		fputs($this->Socket, $query->ToString());
+    public function Send(AsteriskManagerMessage $query) {
+        if ($this->Socket == null) {
+            throw new Exception("Couldn't send query to Asterisk Manager. Null Socket.", constant("AERR_INVALID_SOCKET"));
+        }
 
-		do {
-			$chunk = fgets($this->Socket);
-			$info  = stream_get_meta_data($this->Socket);
-			$resp .= $chunk;
-		} while ($chunk != "\r\n" && $info != "timed_out");
+        //echo "<pre>{$query->ToString()}</pre>";
+        fputs($this->Socket, $query->ToString());
 
-		return new AsteriskManagerMessage($resp);
-	}
-	
-	public function Run($clicmd) {
-		if ($this->Socket == null) {
-			throw new Exception("Couldn't send query to Asterisk Manager. Null Socket.", constant("AERR_INVALID_SOCKET"));
-		}
-		
-		$query = new AsteriskManagerMessage();
-		$query->SetKey('Action' , 'Command');
-		$query->SetKey('Command', $clicmd);
-		
-		fputs($this->Socket, $query->ToString());
-		
-		$chunk_no = 0;
-		do {
-			$chunk = fgets($this->Socket);
-			$info  = stream_get_meta_data($this->Socket);
-			if ($chunk_no > 2) {
-				if ($chunk != "--END COMMAND--\r\n")
-					$resp .= $chunk;
-			}
-			$chunk_no++;
-		} while ($chunk != "\r\n" && $info != "timed_out");
-		
-		return  $resp;
-	}
+        $resp = "";
+        do {
+            $chunk = fgets($this->Socket);
+            $info = stream_get_meta_data($this->Socket);
+            $resp .= $chunk;
+        } while ($chunk != "\r\n" && $info != "timed_out");
+
+        return new AsteriskManagerMessage($resp);
+    }
+
+    public function Run($clicmd) {
+        if ($this->Socket == null) {
+            throw new Exception("Couldn't send query to Asterisk Manager. Null Socket.", constant("AERR_INVALID_SOCKET"));
+        }
+
+        $query = new AsteriskManagerMessage();
+        $query->SetKey('Action', 'Command');
+        $query->SetKey('Command', $clicmd);
+
+        fputs($this->Socket, $query->ToString());
+
+        $chunk_no = 0;
+        $resp = "";
+        do {
+            $chunk = fgets($this->Socket);
+            $info = stream_get_meta_data($this->Socket);
+            if ($chunk_no > 2) {
+                if ($chunk != "--END COMMAND--\r\n") {
+                    $resp .= $chunk;
+                }
+            }
+            $chunk_no++;
+        } while ($chunk != "\r\n" && $info != "timed_out");
+
+        return $resp;
+    }
+
 }
+
 ?>
